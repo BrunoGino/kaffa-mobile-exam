6c5a18fac63915682ed0cc88fb3690da3aeeb30d
Bruno Gino <bruno.gino24@gmail.com>
2020-06-08 22:21:57 -0300

    Added CNPJ validator and blank checks

--- questions-one-to-four/src/questionone/CNPJ.java
+++ questions-one-to-four/src/questionone/CNPJ.java
@@ -1,10 +1,18 @@
 package questionone;
 
+import questionone.utils.CNPJUtils;
+
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
+import java.util.List;
+
 public class CNPJ {
     private String value;
+    private CNPJUtils cnpjUtils;
 
     public CNPJ(String value) {
         this.value = value;
+        this.cnpjUtils = new CNPJUtils();
     }
 
     public String getValue() {
@@ -12,11 +20,58 @@ public class CNPJ {
     }
 
     public void setValue(String value) {
-        this.value = value;
+        if (isStringLikeCNPJ()) {
+            this.value = value.trim();
+        }
     }
 
-    public boolean hasMask() {
-        return false;
+    public boolean isStringLikeCNPJ() {
+        try {
+            return isNotBlankStringLikeCNPJ();
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
+    private boolean isNotBlankStringLikeCNPJ() {
+        if (value == null || value.isEmpty()) {
+            return false;
+        } else {
+            return isCNPJConvertibleToLong(value.trim());
+        }
+    }
+
+    private boolean isCNPJConvertibleToLong(String cnpjValue) {
+        if (cnpjValue.length() == 14) {
+            cnpjToLong(cnpjValue);
+            return true;
+        } else if (cnpjValue.length() == 18 && hasMask(cnpjValue)) {
+            String collect = getCnpjUnformatted(cnpjValue);
+            cnpjToLong(collect);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getCnpjUnformatted(String cnpjValue) {
+        List<String> characters = cnpjUtils.parseArrayOfCharsAsListOfString(cnpjValue);
+        characters.removeIf(character -> character.equals(".") || character.equals("/") || character.equals("-"));
+        return String.join("", characters);
+    }
+
+    private Long cnpjToLong(String value) {
+        return Long.parseLong(value);
+    }
+
+    public boolean hasMask(String cnpjValue) {
+        try {
+            MaskFormatter maskFormatter = new MaskFormatter("##.###.###/####-##");
+            String formattedCnpj = maskFormatter.valueToString(cnpjValue);
+            return cnpjValue.equals(formattedCnpj);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- questions-one-to-four/test/java/questionone/CNPJTest.java
+++ questions-one-to-four/test/java/questionone/CNPJTest.java
@@ -3,24 +3,79 @@ package questionone;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class CNPJTest {
 
     @Test
-    public void must_CheckIfCNPJIsValid_WhenStringIsFormatted() {
-        CNPJ cnpj = new CNPJ("00.000.000/0000-00");
+    public void must_CheckIfFormattedCnpj_IsValid() {
+        CNPJ cnpj = new CNPJ("32.290.342/0001-80");
         CNPJValidator cnpjValidator = new CNPJValidator();
-
-        assertTrue(cnpjValidator.isCnpjValid(cnpj));
+        assertThat(cnpjValidator.isCnpjValid(cnpj), equalTo(true));
     }
 
     @Test
-    public void must_CheckIfCNPJIsValid_WhenStringIsNotFormatted(){
-        CNPJ cnpj = new CNPJ("00000000000000");
+    public void must_CheckIfCNPJ_IsValid() {
+        CNPJ cnpj = new CNPJ("32290342000180");
         CNPJValidator cnpjValidator = new CNPJValidator();
+        assertThat(cnpjValidator.isCnpjValid(cnpj), equalTo(true));
+    }
 
-        assertTrue(cnpjValidator.isCnpjValid(cnpj));
+    @Test
+    public void must_CheckIfFormattedInvalidCnpj_IsInvalid() {
+        CNPJ cnpj = new CNPJ("12.345.678/9101-12");
+        CNPJValidator cnpjValidator = new CNPJValidator();
+        assertThat(cnpjValidator.isCnpjValid(cnpj), equalTo(false));
+    }
+
+    @Test
+    public void must_CheckIfInvalidCNPJ_isInvalid() {
+        CNPJ cnpj = new CNPJ("12345679910112");
+        CNPJValidator cnpjValidator = new CNPJValidator();
+        assertThat(cnpjValidator.isCnpjValid(cnpj), equalTo(false));
+    }
+
+    @Test
+    public void must_CheckIfStringLooksLikeCNPJ_WhenStringIsFormatted() {
+        CNPJ cnpj = new CNPJ("00.000.000/0000-00");
+
+        assertThat(cnpj.getValue().length(), equalTo(18));
+        assertThat(cnpj.isStringLikeCNPJ(), equalTo(true));
+    }
+
+    @Test
+    public void must_CheckIfStringLooksLikeCNPJ_WhenStringIsNotFormatted() {
+        CNPJ cnpj = new CNPJ("00000000000000");
+        assertTrue(cnpj.isStringLikeCNPJ());
+    }
+
+    @Test
+    public void must_CheckIfStringNotLooksLikeCnpj_WhenLetterIsInString() {
+        CNPJ cnpj = new CNPJ("0000a000000000");
+        assertThat(cnpj.isStringLikeCNPJ(), equalTo(false));
+    }
+
+    @Test
+    public void must_CheckIfStringNotLooksLikeCnpj_WhenStringIsGreaterThanExpected() {
+        CNPJ cnpj = new CNPJ("0000000000000000");
+        assertThat(cnpj.isStringLikeCNPJ(), equalTo(false));
+    }
+
+    @Test
+    public void must_CheckIfStringNotLooksLikeCnpj_WhenStringIsLessThanExpected() {
+        CNPJ cnpj = new CNPJ("00000000000");
+        assertThat(cnpj.isStringLikeCNPJ(), equalTo(false));
+    }
+
+    @Test
+    public void must_CheckIfStringNotLooksLikeCNPJ_WhenStringIsEmptyOrNull() {
+        CNPJ cnpj = new CNPJ("");
+        CNPJ cnpj2 = new CNPJ(null);
+
+        assertThat(cnpj.isStringLikeCNPJ(), equalTo(false));
+        assertThat(cnpj2.isStringLikeCNPJ(), equalTo(false));
     }
 
 }
